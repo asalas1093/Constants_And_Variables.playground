d7aec33b2e0c8b8970783397617f7be20e6cb425
asalas1093 <asalas@e112-m-05.mchenry.edu>
2015-09-01 17:45:16 -0500

    Constant_And_VariablesAlejandroS

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Experiment - create 3 variables
+
+var int1 = 10
+
+var intF = 2.9
+
+var intD = 99.99
+
+//Experiment - create 3 constants
+
+let intC = 5
+
+let intF2 = 3.9
+
+let intD2 = 100.99
+
+//Experiment - create 3 eplicit constants
+
+let con1 :String = "This is my constant"
+
+let intF3 :Float = 2.99
+
+let intD3 :Double = 1000.99
+
+//Experiment - change constant
+
+let intC2 = 12
+
+//let intC2 = 13 //Error says cannot change a constant
+
+//Experiment - create explicit type of float
+
+let intC3 :Float = 3.99
+
+//let intC3 :Float = 4
+
+//It does not allow you to change the constant
